4f7f1573c734d218beb000fa0c8c1be1376c2c09
Víctor Alfonso Sánchez Pachón <vasanchezp@uqvirtual.edu.co>
2025-03-25 21:22:17 -0500

    modificacion en el main con un nuevo metodo

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -4,4 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos al laboratorio de GIT con Java");
 	}
+    public static void saludo(String nombre) {
+    	System.out.println("Hola, " + nombre + "Espero que disfrutes el laboratorio");
+}
+
+ 
 }
