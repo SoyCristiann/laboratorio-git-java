88dfeace80a3f8cc21e6dab547877e151d2c3544
fabesrc <rodriguezfabes@gmail.com>
2025-03-25 21:19:59 -0500

    Se crea metodo dentro de la clase main

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -4,4 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos al laboratorio de GIT con Java");
 	}
+	
+	public void hello() {
+		System.out.println("Hello");
+	}
+	
 }
